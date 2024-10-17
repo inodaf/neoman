58c48938a2879877c04901eaaaff662a90f38455
Isac Fadoni <isacfadoni@gmail.com>
2024-10-17 23:57:37 +0200

    Define AppConfigDir and DocsRegistryDir.

---
+++ internal/config.go
@@ -0,0 +1,46 @@
+package internal
+
+import (
+	"os"
+	"path"
+)
+
+const AppName = "neoman"
+const ShortAppName = "nman"
+
+// AppConfigDir returns the directory path that used
+// for storing internal app support files.
+// It creates the directory if that was not done before.
+func AppConfigDir() (string, error) {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+
+	appConfigDir := path.Join(configDir, AppName)
+	if _, err := os.Stat(appConfigDir); err != nil {
+		if err := os.Mkdir(appConfigDir, os.ModePerm); err != nil {
+			return "", err
+		}
+	}
+
+	return appConfigDir, nil
+}
+
+// DocsRegistryDir returns the directory path that stores
+// copies of docs. It creates the dir if that was not done before.
+func DocsRegistryDir() (string, error) {
+	appConfigDir, err := AppConfigDir()
+	if err != nil {
+		return "", err
+	}
+
+	baseDir := path.Join(appConfigDir, "registry")
+	if _, err := os.Stat(baseDir); err != nil {
+		if err := os.Mkdir(baseDir, os.ModePerm); err != nil {
+			return "", err
+		}
+	}
+
+	return baseDir, nil
+}
