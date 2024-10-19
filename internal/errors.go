5970c32ab8d5a4ac6b39b78452f4aa9337ceb812
Isac Fadoni <isacfadoni@gmail.com>
2024-10-19 21:48:53 +0200

    Errors: Add AccessRegistryDir error

--- internal/errors.go
+++ internal/errors.go
@@ -2,7 +2,10 @@ package internal
 
 import "errors"
 
-var ErrGetWd = errors.New("neoman: Could not get current working directory")
-var ErrReadDocsDir = errors.New("neoman: No 'docs/' or 'manual/' in this directory")
-var ErrGitNotInstalled = errors.New("neoman: Git is not installed or was not found")
-var ErrNotAGitRepository = errors.New("neoman: Directory '%s' is not a Git repository")
+var (
+	ErrGetWd             = errors.New("neoman: Could not get current working directory")
+	ErrReadDocsDir       = errors.New("neoman: No 'docs/' or 'manual/' in this directory")
+	ErrGitNotInstalled   = errors.New("neoman: Git is not installed or was not found")
+	ErrNotAGitRepository = errors.New("neoman: Directory '%s' is not a Git repository")
+	ErrAccessRegistryDir = errors.New("neoman: Could not access documentation registry")
+)
