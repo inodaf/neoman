2a59cfc06cb0c3c532d2e92bed397a73ba25184e
Isac Fadoni <isacfadoni@gmail.com>
2026-04-13 23:58:27 +0200

    [Add] Daemon/Controller: ListDocPages endpoint to retrieve documentation pages with YAML formatting

---
+++ internal/daemon/controller/listdocpages.go
@@ -0,0 +1,62 @@
+package controller
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"strings"
+
+	"github.com/inodaf/neoman/internal/daemon/domain"
+	"github.com/inodaf/neoman/internal/daemon/usecase"
+)
+
+func (c *controller) ListDocPages(w http.ResponseWriter, r *http.Request) {
+	author := r.PathValue("author")
+	repo := r.PathValue("repo")
+
+	accept := r.Header.Get("Accept")
+	if accept != "" && accept != "application/yaml" {
+		http.Error(w, "Unsupported media type. Use Accept: application/yaml", http.StatusNotAcceptable)
+		return
+	}
+
+	output, err := c.useCase.ListDocPages(usecase.ListDocPagesInput{
+		Author:     author,
+		Repository: repo,
+	})
+
+	switch err {
+	case nil:
+		if len(output.Pages) == 0 {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		w.Header().Set("Content-Type", "application/yaml")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(formatPagesAsYAML(author, repo, output.Pages)))
+		return
+	case usecase.ErrListDocPagesDocsNotFound:
+		http.Error(w, "Docs not found", http.StatusNotFound)
+		return
+	case usecase.ErrListDocPagesAuthorRequired, usecase.ErrListDocPagesRepositoryRequired:
+		http.Error(w, "Missing author or repository", http.StatusBadRequest)
+		return
+	default:
+		slog.Error("failed to list doc pages", "error", err)
+		http.Error(w, "Unable to list doc pages", http.StatusInternalServerError)
+		return
+	}
+}
+
+func formatPagesAsYAML(author, repo string, pages []domain.DocsPage) string {
+	var sb strings.Builder
+
+	sb.WriteString(fmt.Sprintf("project: %s/%s\n", author, repo))
+	sb.WriteString("docs:\n")
+
+	for _, page := range pages {
+		sb.WriteString(fmt.Sprintf("  - %s: '%s'\n", page.Title, page.RelativePath))
+	}
+
+	return sb.String()
+}
